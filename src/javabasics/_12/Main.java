4e890acfad666440e6ffecd8efeabb57311327e7
Fab11 <fab.stazi11@gmail.com>
2023-03-13 13:36:40 +0100

    Recap challenges!!

--- src/javabasics/_12/Main.java
+++ src/javabasics/_12/Main.java
@@ -25,6 +25,21 @@ public class Main {
         double yourScore = 0;
 
         // Start your code here
+        yourScore = text.length(); //così il mio punteggio è uguale alla lunghezza del testo!!
+
+        if (yourScore > 20){
+            yourScore = 20; //se è mag di 20 allora lo rimetto a 20
+        }
+        if(text.contains("a")){
+         yourScore -= 5; //se contiene la lettera "a" (il testo) allora sono -5 pt!
+        }
+        if (text == text.toLowerCase()){
+            yourScore += 10; //se il testo è tutto in minuscolo allora sono +10 pT!!
+        }
+        if(yourScore > 20){
+            yourScore *= 2; //se ora il mio punteggio è maggiore di 20 lo moltiplico x2!!
+        }
+
 
         // End it here
 
@@ -48,6 +63,15 @@ public class Main {
         double yourMealScore = 0;
         // Start your code here
 
+         if (mealType.contains("sandwich")){
+             weight *=5; //peso sandwich moltiplicato x5!
+         } else if (mealType.contains("soup") ){
+             weight *= 3;   //peso zuppa moltiplicato x3!
+             if(weight < 5){ //se il peso è min di 5 allora lo faccio diventare 5!
+                 weight = 5;
+             }
+         }
+         yourMealScore = weight; //così il mio punteggio è uguale al peso!
         // End it here
 
         System.out.print("Expected score=" + expectedScore +", actual score=" + yourMealScore);
