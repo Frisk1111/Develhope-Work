24faae8c601ab9515ba5a9b65c3ec38986e7d963
Frisk <fab.stazi11@gmail.com>
2023-03-28 11:31:56 +0200

    javaadvanced_2 finito - Map!

--- src/javaadvanced/_2/Exercises.java
+++ src/javaadvanced/_2/Exercises.java
@@ -18,10 +18,34 @@ public class Exercises {
         Map<Integer, String> myFirstMap = new HashMap<>();
 
         // 1b. Add 3 elements to the map .put
+
+        myFirstMap.put(30, "NY");
+        myFirstMap.put(69, "Bangkok");  //a differenza delle array list e dei set
+        myFirstMap.put(43, "Tokyo");   // per aggiungere elementi alla Map bisogna fare '.put'!!
+
+
+        //end
+
         // 1c. Print the map
+        System.out.println(myFirstMap); //così stampa TUTTI gli elementi della map!
+        //end
+
         // 1d. Access an element in the map with .get
+
+        System.out.println(myFirstMap.get(30)); //così stampa SOlO la value della key '30'!!
+
+        //end
+
         // 1e. Remove an element from the map .remove
+
+        myFirstMap.remove(30); //sia nel .remove che nel .get va messo la "key" -
+        // qua eliminerà la key '30' e la value associata!
+
+        //end
+
         // 1f. Print the map again to confirm the element was removed
+        System.out.println(myFirstMap);
+        //end
     }
 
     /**
@@ -42,6 +66,40 @@ public class Exercises {
         //     to do so
 
         // Your code
+
+
+
+        //do a entry tutti i valori della Map -quindi sia il valore della key che del value
+        for(Map.Entry<String, Double> entry : menuCostsInEuro.entrySet()){
+
+
+            //nel .get va messo una key - che in questo caso equivale a una stringa - invece di mettere
+            //una frase ci metto dentro il valore di 'mostExpensiveMenuItemName'
+
+            if(mostExpensiveMenuItemName == null ||
+                    entry.getValue() > menuCostsInEuro.get(mostExpensiveMenuItemName) ){
+                   // ^
+                //qua sarebbe come dire: "se la value di entry e maggiore di menuCosts
+                // - a cui abbiamo messo tra le parentesi il valore di mostExpensive che equivale a null -
+                // allora dai a mostExpensive il valore della key di entry!
+
+
+                //così assume il valore della Key di entry - la key che ha il valore maggiore tra tutti
+              mostExpensiveMenuItemName = entry.getKey();
+
+
+
+
+            }
+
+
+        }
+
+        System.out.println("\n risultato esercizio 2a --> " + mostExpensiveMenuItemName);
+
+
+
+        //end.
 
         if (mostExpensiveMenuItemName.equals("Pizza")) {
             System.out.println("2a. Correct!");
@@ -69,6 +127,18 @@ public class Exercises {
         // 3a. Use a for loop with fruitWeights.values() and sum up the total weight
 
         // Your code
+
+        for (Map.Entry<String, Integer> pesoFrutta : fruitWeights.entrySet()) {
+
+            //assegno a 'totalWeight' i value di 'pesoFrutta' --gli stessi presenti nella map 'fruitWeights'
+            // e l'operatore '+' prima dell'uguale farà in modo di sommare tra loro dentro il loop
+            // tutti i value di 'pesoFrutta'!
+            totalWeight += pesoFrutta.getValue();
+
+        }
+
+        System.out.println("\n Risultato esercizio 3 --> " + totalWeight);
+        //end!
 
         if (totalWeight == fruitWeights.values().stream().mapToInt(Integer::intValue).sum()) {
             System.out.println("3. Correct!");
