f22fe4631cbdb36c239068089a73b30bdca1fdc3
Frisk <fab.stazi11@gmail.com>
2023-04-02 19:05:10 +0200

    javaadvanced_5- risolvere l'es 4!! il resto va più che bene!

--- src/javaadvanced/_5/Exercises.java
+++ src/javaadvanced/_5/Exercises.java
@@ -1,29 +1,118 @@
 package javaadvanced._5;
 
+
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Exercises {
     public static void main(String[] args) {
-        exercise1();
-        exercise2();
-        exercise3();
-        exercise4();
+
+
+        //qua invece mi crea il file senza stampare l'errore - penso vada bene?
+        //tanto chiedeva di mettere il printStack nel catch
+        // -
+        // -->>massi va bene!
+//        exercise1();
+
+        // usato metodo 'readString' - ma non so se sia giusto
+        //mi lancia il catch però non so se conta come 'aprire il file'
+        //come diceva nella traccia-
+        // -
+        // -->>facciamocelo andar bene dai- funziona almeno!
+//        exercise2();
+
+        // -->>manda perfettamente il messaggio del catch!!
+//        exercise3();
+
+
+        //OK trovato giusto metodo per il secondo catch ma invece di stamparmi i catch mi dà
+        // -->>il risultato della divisione - mi dà "infinity" :|
+//       exercise4();
     }
 
     /**
      * 1: Uncomment the Files.createFile() line below and write a try catch block that
-     *    prints its stack trace
+     * prints its stack trace
      */
     private static void exercise1() {
         System.out.println("Exercise 1: ");
         // Your code here
-        try{
+
+        String avvisoProblema = "\n Errore! Qualcosa è andato storto! :( \n";
+
+        try {
             Files.createFile(Path.of("Test.txt"));
 
-        } catch(Exception exception) {
+        } catch (Exception exception) {
 
-            System.err.println("Errore! Qualcosa è andato storto! :(");
+            System.err.println(avvisoProblema.toUpperCase());
+            exception.printStackTrace();
+            System.exit(0);
+        }
+        //end
+
+    }
+
+    /**
+     * 2:
+     * *.
+     * Write a try-catch block that attempts to open a file
+     * *.
+     * In the catch block, print a message that informs the user that the file could not be found.
+     */
+    private static void exercise2() {
+        System.out.println("\nExercise 2: ");
+
+        //messo qua sopra altrimenti
+        // non me l'avrebbe rilevato nel blocco try-catch!
+        String userInputFileName = "test-file.txt";
+
+        Path percorsoFile = Paths.get(userInputFileName);
+
+        // Your code here
+
+        String avviso = "\n Attenzione! File inesistente! \n";
+
+        try {
+
+            Files.readString(percorsoFile);
+
+
+        } catch (Exception exception) {
+
+            System.err.println(avviso.toUpperCase());
+            exception.printStackTrace();
+            System.exit(0);
+
+        }
+
+        //end
+
+
+    }
+
+    /**
+     * 3:
+     * .
+     * Write a try-catch block that attempts to parse a string as an integer.
+     * .
+     * In the catch block, print a message that informs the user that the input was not a valid integer.
+     */
+    private static void exercise3() {
+        System.out.println("\nExercise 3: ");
+        // Your code here
+
+        String avviso = "\n Attenzione! Input non valido :( \n sicuro che sia un numero intero? \n ";
+
+        try {
+
+            Integer.parseInt("house");
+
+        } catch (Exception exception) {
+
+            System.err.println(avviso.toUpperCase());
             exception.printStackTrace();
             System.exit(0);
         }
@@ -31,55 +120,50 @@ public class Exercises {
     }
 
     /**
-     * 2:
-     **.
-     * Write a try-catch block that attempts to open a file
-     **.
-     * In the catch block, print a message that informs the user that the file could not be found.
-     */
-    private static void exercise2() {
-        System.out.println("\nExercise 2: ");
-        // Your code here
-
-        //end
-
-        String userInputFileName = "test-file.txt";
-    }
-
-    /**
-     * 3:
-     *.
-     * Write a try-catch block that attempts to parse a string as an integer.
-     *.
-     * In the catch block, print a message that informs the user that the input was not a valid integer.
-     */
-    private static void exercise3() {
-        System.out.println("\nExercise 3: ");
-        // Your code here
-
-        Integer.parseInt("house");
-    }
-
-    /**
      * 4:
-     *.
+     * .
      * Write a try block that around this print statement that attempts to divide 2 numbers
-     *.
+     * .
      * Create multiple catch blocks that catch different types of exceptions, such as NumberFormatException and DivideByZeroException.
-     *.
+     * .
      * In each catch block, print a message that informs the user of the specific exception that was caught and why it occurred.
-     *
      */
     private static void exercise4() {
         System.out.println("\nExercise 4: ");
         // Your code here
 
-        Double num1 = 10.0;
+
+        double num1 = 10.0;
         String num2AsString = "0.0";
-        // try{
-            System.out.println(num1/Double.parseDouble(num2AsString));
-        //}
-        // Your catch blocks here
+
+        String avvisoFormato = "\n Attenzione! Le due variabili devono avere lo stesso formato ! \n";
+
+        String numeroDivisoPerZero = "\n Non puoi dividere un numero per zero! Cambia il valore di quella variabile!";
+
+
+        try {
+
+            //mi stampa 'infitnity'...
+            //BOH
+            System.out.println(num1 / Double.parseDouble(num2AsString));
+
+            //Your catch blocks here
+        } catch (NumberFormatException numberFormatException) {
+
+            System.err.println(avvisoFormato.toUpperCase());
+            numberFormatException.printStackTrace();
+
+            //questo dovrebbe servire per le operazioni come quella di sopra!
+        } catch (ArithmeticException divideByZeroException) {
+
+            System.err.println(numeroDivisoPerZero.toUpperCase());
+            divideByZeroException.printStackTrace();
+            System.exit(0);
+
+        }
+
+
+
 
     }
 }
